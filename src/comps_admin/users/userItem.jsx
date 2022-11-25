9bb803414e5103fe5733705f47b25876fe02cb04
Chaya Segal <chayale90@gmail.com>
2022-11-25 14:23:32 +0200

    aaaa

--- src/comps_admin/users/userItem.jsx
+++ src/comps_admin/users/userItem.jsx
@@ -1,23 +1,73 @@
 import React from 'react'
+import { API_URL, doApiMethod } from '../../services/apiService';
 
 export default function UserItem(props) {
     let item = props.item;
 
-
     const onRoleClick = async () => {
         let bodyData;
+        if (item.role == "user") {
+            bodyData = { role: "admin" }
+        }
+        else {
+            bodyData = { role: "user" }
+        }
 
-
-
-
+        let url = API_URL + "/users/changeRole/" + item._id
+        try {
+            let resp = await doApiMethod(url, "PATCH", bodyData)
+            console.log(resp.data);
+            if (resp.data) {
+                props.doApi();
+            }
+        }
+        catch (err) {
+            console.log(err);
+            alert("There problem, or you try to change superAdmin to user")
+        }
     }
 
 
+    const onActiveClick = async () => {
+        let bodyData;
+        if (item.active == true) {
+            bodyData = { active: false }
+        }
+        else {
+            bodyData = { active: true }
+        }
+
+        let url = API_URL + "/users/changeActive/" + item._id
+        try {
+            let resp = await doApiMethod(url, "PATCH", bodyData)
+            console.log(resp.data);
+            if (resp.data) {
+                props.doApi();
+            }
+        }
+        catch (err) {
+            console.log(err);
+            alert("There problem, or you try to change superAdmin to user")
+        }
+    }
 
 
-
-
-
+    const onDeleteClick = async () => {
+        let url = API_URL + "/users/" + item._id
+        try {
+            if (window.confirm("Are you sure you want delete this user?")) {
+                let resp = await doApiMethod(url, "DELETE")
+                console.log(resp.data);
+                if (resp.data) {
+                    props.doApi();
+                }
+            }
+        }
+        catch (err) {
+            console.log(err);
+            alert("There problem, or you try to change superAdmin to user")
+        }
+    }
 
     return (
         <tr>
@@ -27,17 +77,15 @@ export default function UserItem(props) {
             <td>{item.sex}</td>
             <td>{item.height}</td>
             <td>{item.weight}</td>
-            <td>{item.role}</td>
+            <td><button className='btn btn-success' onClick={onRoleClick}>{item.role}</button></td>
             <td>{item.rank}</td>
             <td>{item.score}</td>
             <td>{<img src={item.img_url} alt="Avatar" width={60} />}</td>
             <td>{item.nickname}</td>
-            <td>{String(item.active)}</td>
+            <td><button className='btn btn-warning' onClick={onActiveClick}>{String(item.active)}</button></td>
             <td>
-                <button className='badge bg-danger'>Del</button>
+                <button onClick={onDeleteClick} className='badge bg-danger'>Del</button>
             </td>
-
-
         </tr>
     )
 }
