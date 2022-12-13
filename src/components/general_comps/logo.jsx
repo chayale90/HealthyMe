c4f19456244ef95a7c4ed5e6a8ca623600c4ce7f
Chaya Segal <chayale90@gmail.com>
2022-12-13 12:56:21 +0200

    aaa

--- src/components/general_comps/logo.jsx
+++ src/components/general_comps/logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Logo() {
   return (
     <div >
-      <img src="/images/logo1.png" alt="Logo" width={"80"} />
+      <img src="/images/logo.png" alt="Logo" width={"80"} />
     </div>
   )
 }
