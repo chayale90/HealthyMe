d981e15816b39e42a972e80be8ef5f24618ae7a2
Chaya Segal <chayale90@gmail.com>
2022-12-21 15:04:35 +0200

    fix bug

--- src/components/client/foods/foodItem.jsx
+++ src/components/client/foods/foodItem.jsx
@@ -6,13 +6,13 @@ import "./foodItem.css"
 import { useState } from 'react';
 import { API_URL, doApiGet } from '../../../services/apiService';
 import { toast } from 'react-toastify';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function FoodItem({ item }) {
   const [showIcon, setShowIcon] = useState(<FavoriteBorderIcon />);
-  const [userName, setUserName] = useState([]);
-  const [userImg, setUserImg] = useState([]);
-  // console.log(userInfo);
+  const [userName, setUserName] = useState("");
+  const [userImg, setUserImg] = useState("");
+  const nav = useNavigate();
 
   useEffect(() => {
     doApiGetInfoUser();
@@ -22,7 +22,7 @@ export default function FoodItem({ item }) {
     try {
       let url = API_URL + "/users/userInfo/" + item.user_id;
       const resp = await doApiGet(url);
-      console.log(resp.data);
+      // console.log(resp.data);
       setUserName(resp.data.name);
       setUserImg(resp.data.img_url)
     }
@@ -32,40 +32,70 @@ export default function FoodItem({ item }) {
     }
   }
 
+
+  const onLikeClick = async () => {
+    setShowIcon(<FavoriteIcon sx={{ color: "red" }} />)
+    let bodyData;
+    if (item.role == "user") {
+      bodyData = { role: "admin" }
+    }
+    else {
+      bodyData = { role: "user" }
+    }
+    let url = API_URL + "/users/changeRole/" + item._id
+    try {
+      let resp = await doApiMethod(url, "PATCH", bodyData)
+      console.log(resp.data);
+      if (resp.data) {
+        props.doApi();
+      }
+    }
+    catch (err) {
+      console.log(err);
+      alert("There problem, or you try to change superAdmin to user")
+    }
+  }
+
   return (
     <React.Fragment>
-
       <div className='col-lg-4 mainDiv p-0 '>
         <div className='p-2 overflow-hidden h-100 '>
           <img className='imgFood w-100' height={"200px"} src={item.img_url} />
 
           <div className='mt-3 d-flex align-items-center justify-content-between w-100'>
 
-            <div className='d-flex align-items-center '>
-              <Avatar sx={{ float: 'start', width: 33, height: 33 }}
-                alt="AvatarOfFood" src={userImg}
-              />
-              <Link style={{
-                textDecoration: "none", fontWeight: 500}} className='s16 ms-2'>{userName}</Link>
-          </div>
-
-          <div>
-            <IconButton
-              onClick={() => {
-                setShowIcon(<FavoriteIcon sx={{ color: "red" }} />)
+            <div className='d-flex align-items-center'
+              style={{
+                cursor: "pointer"
               }}
-              sx={{ width: 33, height: 33 }}
-              aria-label="add to favorites"
-            >
-              {showIcon}
-            </IconButton>
-          </div>
+              onClick={() => {
+                //go to details of user
+                nav("/")
+              }}>
+              <Avatar
+                sx={{ float: 'start', width: 33, height: 33 }}
+                src={userImg} alt="AvatarOfFood"
+              />
+              <div style={{
+                fontWeight: 500
+              }} className='s16 ms-2'>{userName}</div>
+            </div>
 
+            <div>
+              <IconButton
+                onClick={onLikeClick}
+                sx={{ width: 33, height: 33 }}
+                aria-label="add to favorites"
+              >
+                {showIcon}
+              </IconButton>
+            </div>
+
+          </div>
+          <div className='s14 mt-2 ms-1'>{item.name}</div>
+          <div className='s14 ms-1'>{item.likes.length} likes</div>
         </div>
-        <div className='s14 mt-2 ms-1'>{item.name}</div>
-        <div className='s14 ms-1'>{item.likes.length} likes</div>
       </div>
-    </div>
     </React.Fragment >
   )
 }
