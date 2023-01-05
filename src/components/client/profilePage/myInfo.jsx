fa39b921a47407bfa86de4d9c70d6202a6675ac8
Chaya Segal <chayale90@gmail.com>
2023-01-05 02:26:27 +0200

    <br>

--- src/components/client/profilePage/myInfo.jsx
+++ src/components/client/profilePage/myInfo.jsx
@@ -9,8 +9,8 @@ export default function MyInfo() {
     <div className='container '>
       <div  className='row text-center justify-content-center'>
       <div className='divCount py-4 mx-3'>{user.height/100} <span className='weight500'> Height</span> </div>
-      <div className='divCount py-4 mx-3'>{user.weight} <span className='weight500'>KG</span> </div>
-      <div className='divCount py-4 mx-3'>{user.weight / user.height * user.height} <span className='weight500'>BMI</span> </div>
+      <div className='divCount py-4 mx-3'>{user.weight} <br/><span className='weight500'>KG</span> </div>
+      <div className='divCount py-4 mx-3'>{user.weight / user.height * user.height} <br/><span className='weight500'>BMI</span> </div>
       </div>
     </div>
   )
