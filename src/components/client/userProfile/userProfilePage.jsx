24d27fa07776d25586e8e23c9a0d7fb0d01615c4
Chaya Segal <chayale90@gmail.com>
2023-01-10 16:21:33 +0200

    userProfilePage

--- src/components/client/userProfile/userProfilePage.jsx
+++ src/components/client/userProfile/userProfilePage.jsx
@@ -7,14 +7,17 @@ import CheckUserComp from '../../auth/checkComps/checkUserComp';
 import { changeFavorites, changeHome } from "../../../features/homeSlice"
 import { setOpenFollowers, setOpenFollowings } from "../../../features/dialogSlice"
 import { useEffect } from 'react';
-import { API_URL, doApiGet } from '../../../services/apiService';
+import { API_URL, doApiGet, doApiMethod } from '../../../services/apiService';
 import CheckUserActiveComp from '../../auth/checkComps/checkUserActiveComp';
 import { toast } from 'react-toastify';
 import UserPostsList from './userPostsList';
-import { btnStyle2 } from '../../../services/btnStyle';
+import { btnStyle2, btnStyle3 } from '../../../services/btnStyle';
+
 
 export default function UserProfilePage() {
-  const [user, setUser] = useState({})
+  const { user } = useSelector(myStore => myStore.userSlice);
+
+  const [otherUser, setOtherUser] = useState({})
   const nav = useNavigate()
   // console.log(user);
   const dispatch = useDispatch();
@@ -31,22 +34,14 @@ export default function UserProfilePage() {
       const url = API_URL + "/users/userInfo/" + params["id"];
       const resp = await doApiGet(url);
       console.log(resp.data);
-      setUser(resp.data);
+      setOtherUser(resp.data);
+
     }
     catch (err) {
       console.log(err);
       toast.error("There problem try come back later");
     }
   };
-
-  // const clickOnPosts = () => {
-  //   setValues({
-  //     button1: "#A435F0",
-  //     button2: "#CCCCCC",
-  //   });
-  //   setShowPosts("block")
-  //   setShowInfo("none")
-  // }
 
   const onClickFollowers = () => {
     dispatch(setOpenFollowers({ val: true }))
@@ -56,24 +51,38 @@ export default function UserProfilePage() {
     dispatch(setOpenFollowings({ val: true }))
   }
 
+  const onFollowClick = async () => {
+    try {
+      const url = API_URL + "/users/changeFollow/" + params["id"];
+      let resp = await doApiMethod(url, "PATCH")
+      console.log(resp.data);
+      if(resp.data){
+        doApi()
+      }
+    }
+    catch (err) {
+      console.log(err);
+      toast.error("There problem try come back later");
+    }
+  }
 
   return (
     <div>
       <div className='container mt-md-5 mt-4'>
         <CheckUserActiveComp />
-        <div className='d-flex '>
+        <div className='d-flex'>
 
-          <div className='d-none d-sm-block justify-content-between'>
+          <div className='d-none d-sm-block '>
             <Avatar
               alt="myAvater"
-              src={user.img_url}
+              src={otherUser?.img_url}
               sx={{ width: 160, height: 160 }}
             />
           </div>
           <div className='d-block d-sm-none'>
             <Avatar
               alt="myAvater"
-              src={user.img_url}
+              src={otherUser?.img_url}
               sx={{ width: 70, height: 70 }}
             />
           </div>
@@ -81,14 +90,15 @@ export default function UserProfilePage() {
           <div className='ms-md-5 ms-2 mt-0 mt-sm-1'>
             <div className='d-flex'>
 
-              <h2 className='mb-3 s24'> {user?.name} |<span className='purple'> {user?.rank}</span> </h2>
-              <div className='ms-auto justify-content-end d-md-none d-block'>
+              <h2 className='mb-3 s24'> {otherUser?.name} |<span className='purple'> {otherUser?.rank}</span> </h2>
+              <div className=' d-md-none d-block'>
                 <Button
+                  onClick={onFollowClick}
                   style={{ float: "right" }}
-                  className=' loginBtn s18'
-                  sx={btnStyle2}
+                  className=' loginBtn'
+                  sx={ btnStyle2 }
                 >
-                  Follow
+                 follow
                 </Button>
               </div>
 
@@ -97,7 +107,7 @@ export default function UserProfilePage() {
 
               <div
                 className='me-3'>
-                {user?.posts?.length} <span className='weight500'>
+                {otherUser?.posts?.length} <span className='weight500'>
                   Posts
                 </span>
               </div>
@@ -105,7 +115,7 @@ export default function UserProfilePage() {
               <div style={{ cursor: "pointer" }}
                 onClick={onClickFollowers}
                 className='underLine me-3'>
-                {user?.followers?.length} <span className='weight500'>
+                {otherUser?.followers?.length} <span className='weight500'>
                   Followers
                 </span>
               </div>
@@ -113,29 +123,36 @@ export default function UserProfilePage() {
               <div style={{ cursor: "pointer" }}
                 onClick={onClickFollowings}
                 className='underLine '>
-                {user?.followings?.length} <span className='weight500'>
+                {otherUser?.followings?.length} <span className='weight500'>
                   Followings </span>
               </div>
             </div>
 
-            <div className='pb-2'>My motto: {user?.info}</div>
-            <div className='pb-2'>Location: {user?.location}</div>
+            <div className='pb-2'>My motto: {otherUser?.info}</div>
+            <div className='pb-2'>Location: {otherUser?.location}</div>
 
-            <div>Coins: {user?.score} <AttachMoneyIcon sx={{ color: '#DAA520' }} /></div>
+            <div>Coins: {otherUser?.score} <AttachMoneyIcon sx={{ color: '#DAA520' }} /></div>
 
           </div>
 
 
-          <div className='ms-auto justify-content-end d-none d-md-block '>
-                <Button
-                  style={{ float: "right"}}
-                  className=' loginBtn px-4'
-                  sx={btnStyle2}
-                >
-                  Follow
-                </Button>
-              </div>
-              
+          <div className='ms-auto justify-content-end d-none d-md-block ju'>
+            <Button
+              onClick={onFollowClick}
+              style={{ float: "right" }}
+              className='loginBtn px-4'
+              sx={btnStyle2}
+            >
+              {user?.followings?.map(item => {
+                return (
+                  item.includes(params["id"]) ? "Follow" : "Following"
+                )
+              })}
+
+
+            </Button>
+          </div>
+
         </div>
 
         <UserPostsList />
