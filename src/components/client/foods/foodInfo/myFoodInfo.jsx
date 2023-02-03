78691da557da6b4bede98cca55818c1e49b684af
Chaya Segal <chayale90@gmail.com>
2023-02-03 14:38:12 +0200

    myFoodInfo loading

--- src/components/client/foods/foodInfo/myFoodInfo.jsx
+++ src/components/client/foods/foodInfo/myFoodInfo.jsx
@@ -1,4 +1,4 @@
-import { Button, IconButton } from '@mui/material';
+import { Button, CircularProgress, IconButton } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -90,34 +90,21 @@ export default function MyFoodInfo() {
     };
 
     return (
+        <ThemeProvider theme={theme}>
+            <div className='container mt-md-5 mt-4'>
 
-        <div className='container mt-md-5 mt-4'>
-            {food._id ?
 
 
-                <ThemeProvider theme={theme}>
 
-                    <div style={{ position: 'relative' }} className='row align-items-center'>
-                        <div style={{ position: 'relative' }} className='col-4 d-none d-md-block p-0'>
-                            <img className='imgFoodInfo' style={{ borderRadius: "12px" }} src={food.img_url} alt="foodImg" />
-                            <div className='likesDiv m-1'>  <FavoriteIcon sx={{ color: "#A435F0", marginRight: 1 }} />{food?.likes?.length} Likes</div>
-                        </div>
+                {food._id ?
+                    <div>
+                        <div style={{ position: 'relative' }} className='row align-items-center'>
+                            <div style={{ position: 'relative' }} className='col-4 d-none d-md-block p-0'>
+                                <img className='imgFoodInfo' style={{ borderRadius: "12px" }} src={food.img_url} alt="foodImg" />
+                                <div className='likesDiv m-1'>  <FavoriteIcon sx={{ color: "#A435F0", marginRight: 1 }} />{food?.likes?.length} Likes</div>
+                            </div>
 
-                        <div className="d-flex d-md-none align-items-center">
-                            <Avatar
-                                sx={{ float: "start", width: 33, height: 33 }}
-                                src={userImg}
-                                alt="AvatarOfFood"
-                            />
-                            <Link style={{ fontWeight: 500 }} className="s16 ms-2 dark underLine"
-                                to={(user._id == food.user_id) ? "/myProfile" : "/userProfile/" + food.user_id}
-                            >
-                                {userName}
-                            </Link>
-                        </div>
-
-                        <div className='col-auto mt-2 mt-md-0 mx-auto mx-md-0 ms-md-3 pe-0'>
-                            <div className="d-none d-md-flex align-items-center mb-2 mb-lg-4 mt-lg-2 mt-0">
+                            <div className="d-flex d-md-none align-items-center">
                                 <Avatar
                                     sx={{ float: "start", width: 33, height: 33 }}
                                     src={userImg}
@@ -130,97 +117,120 @@ export default function MyFoodInfo() {
                                 </Link>
                             </div>
 
-                            <h1 className='s30 text-center text-md-start mt-4 mt-md-0'>{food.name}</h1>
-                            <div className='s18 text-center text-md-start mb-4 mb-md-0 mb-lg-4'>{food.description}</div>
-                            <div className='d-flex align-items-center'>
-                                <div className="d-block d-md-flex text-center">
-                                    <div> <AccessTimeIcon className='me-2' /></div>
-                                    prep: {food.prepHours == null || food.prepHours == 0 ? "" : food.prepHours + " hrs"}  {food.prepMinutes} min
+                            <div className='col-auto mt-2 mt-md-0 mx-auto mx-md-0 ms-md-3 pe-0'>
+                                <div className="d-none d-md-flex align-items-center mb-2 mb-lg-4 mt-lg-2 mt-0">
+                                    <Avatar
+                                        sx={{ float: "start", width: 33, height: 33 }}
+                                        src={userImg}
+                                        alt="AvatarOfFood"
+                                    />
+                                    <Link style={{ fontWeight: 500 }} className="s16 ms-2 dark underLine"
+                                        to={(user._id == food.user_id) ? "/myProfile" : "/userProfile/" + food.user_id}
+                                    >
+                                        {userName}
+                                    </Link>
                                 </div>
-                                <hr className='hr' />
 
-                                <div className="d-block d-md-flex text-center"> <img className='me-2 mb-1' src="/images/dishes.png" alt="clockIcon" />
-                                    <div>{food.dishes} dishes</div></div>
-
-                                {food.calories && <hr className='hr' />}
-
-                                {food.calories &&
-                                    <div className='d-block d-md-flex text-center'>
-                                        <div> <img className='me-2 ' src="/images/calories.png" alt="clockIcon" /></div>
-                                        <div >{food.calories} calories</div>
+                                <h1 className='s30 text-center text-md-start mt-4 mt-md-0'>{food.name}</h1>
+                                <div className='s18 text-center text-md-start mb-4 mb-md-0 mb-lg-4'>{food.description}</div>
+                                <div className='d-flex align-items-center'>
+                                    <div className="d-block d-md-flex text-center">
+                                        <div> <AccessTimeIcon className='me-2' /></div>
+                                        prep: {food.prepHours == null || food.prepHours == 0 ? "" : food.prepHours + " hrs"}  {food.prepMinutes} min
                                     </div>
-                                }
+                                    <hr className='hr' />
 
+                                    <div className="d-block d-md-flex text-center"> <img className='me-2 mb-1' src="/images/dishes.png" alt="clockIcon" />
+                                        <div>{food.dishes} dishes</div></div>
+
+                                    {food.calories && <hr className='hr' />}
+
+                                    {food.calories &&
+                                        <div className='d-block d-md-flex text-center'>
+                                            <div> <img className='me-2 ' src="/images/calories.png" alt="clockIcon" /></div>
+                                            <div >{food.calories} calories</div>
+                                        </div>
+                                    }
+
+                                </div>
                             </div>
-                        </div>
-                        <div className='d-block d-md-none mt-4 mx-auto'>
-                            <img className='imgFoodInfo' style={{ borderRadius: "12px", position: 'relative' }} src={food.img_url} alt="foodImg" />
-                            <div className='likesDiv mb-1 ms-3'>  <FavoriteIcon sx={{ color: "#A435F0", marginRight: 1 }} />{food?.likes?.length} Likes</div>
-                        </div>
-                        <div>
-                            <IconButton
-                                style={{ position: 'absolute', right: 0, top: 0 }}
-                                onClick={ClickDelete}
-                            >
-                                < DeleteIcon />
-                            </IconButton>
 
-                            <IconButton
-                                style={{ position: 'absolute', right: 45, top: 0, border: "0.5px solid #A7A7A7" }}
-                            // onClick={""}
-
-                            >
-                                <EditIcon />
-                            </IconButton>
-                        </div>
-
-
-                        <Dialog
-                            open={open}
-                            onClose={handleClose}
-                            aria-labelledby="alert-dialog-title"
-                            aria-describedby="alert-dialog-description"
-                        >
-                            <div className='p-3'>
-                                <DialogTitle
-                                    sx={{ mb: 2 }}
-                                    id="alert-dialog-title">
-                                    Are you sure you want to delete {food.name} dish?
-                                </DialogTitle>
-                                <DialogActions>
-                                    <Button onClick={handleClose}>Disagree</Button>
-                                    <Button onClick={onDeleteFood} autoFocus>Agree</Button>
-                                </DialogActions>
+                            <div className='d-block d-md-none mt-4 mx-auto'>
+                                <img className='imgFoodInfo' style={{ borderRadius: "12px", position: 'relative' }} src={food.img_url} alt="foodImg" />
+                                <div className='likesDiv mb-1 ms-3'>  <FavoriteIcon sx={{ color: "#A435F0", marginRight: 1 }} />{food?.likes?.length} Likes</div>
                             </div>
-                        </Dialog>
-                    </div>
 
-                    <hr className='mt-5 mb-4' />
-                    <div className='mx-md-5'>
-                        <h3 className='s24'>Ingredients</h3>
-                        <p>{food.ingredient}</p>
-                    </div>
-
-                    <div className='mx-md-5 my-5 '>
-                        {food.recipe &&
                             <div>
-                                <h3 className='s24'>Recipe</h3>
-                                <p>{food.recipe}</p>
+                                <IconButton
+                                    style={{ position: 'absolute', right: 0, top: 0 }}
+                                    onClick={ClickDelete}
+                                >
+                                    < DeleteIcon />
+                                </IconButton>
+
+                                <IconButton
+                                    style={{ position: 'absolute', right: 45, top: 0, border: "0.5px solid #A7A7A7" }}
+                                // onClick={""}
+
+                                >
+                                    <EditIcon />
+                                </IconButton>
                             </div>
-                        }
+
+
+                            <Dialog
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="alert-dialog-title"
+                                aria-describedby="alert-dialog-description"
+                            >
+                                <div className='p-3'>
+                                    <DialogTitle
+                                        sx={{ mb: 2 }}
+                                        id="alert-dialog-title">
+                                        Are you sure you want to delete {food.name} dish?
+                                    </DialogTitle>
+                                    <DialogActions>
+                                        <Button onClick={handleClose}>Disagree</Button>
+                                        <Button onClick={onDeleteFood} autoFocus>Agree</Button>
+                                    </DialogActions>
+                                </div>
+                            </Dialog>
+                        </div>
+
+                        <hr className='mt-5 mb-4' />
+                        <div className='mx-md-5'>
+                            <h3 className='s24'>Ingredients</h3>
+                            <p>{food.ingredient}</p>
+                        </div>
+
+                        <div className='mx-md-5 my-5 '>
+                            {food.recipe &&
+                                <div>
+                                    <h3 className='s24'>Recipe</h3>
+                                    <p>{food.recipe}</p>
+                                </div>
+                            }
+                        </div>
+
+
+                        <Fab
+                            sx={{ background: "#A435F0", color: "white", "&:hover": { color: "white", background: "#912CD6" }, position: 'sticky', bottom: 70, left: 1900 }}
+                            onClick={() => { nav("/addFood") }}
+                            aria-label="addFood">
+                            <AddIcon />
+                        </Fab>
+
                     </div>
+                    : 
+                    <div style={{ display: "flex", alignItems: "center", minHeight: '300px' }}>
+                        <div style={{ margin: "0 auto", color: "#A435F0" }}>
+                            <CircularProgress size={"100px"} />
+                        </div>
+                    </div>
+                }
 
-
-
-                    <Fab
-                        sx={{ background: "#A435F0", color: "white", "&:hover": { color: "white", background: "#912CD6" }, position: 'sticky', bottom: 70, left: 1900 }}
-                        onClick={() => { nav("/addFood") }}
-                        aria-label="addFood">
-                        <AddIcon />
-                    </Fab>
-                </ThemeProvider>
-                : <div className='display-1'>fhdhfjsdhjfsdf</div>
-            }
-        </div>
+            </div>
+        </ThemeProvider>
     )
 }
