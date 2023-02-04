241c051d990d172046f876474e1cc6caebcc6cbb
Chaya Segal <chayale90@gmail.com>
2023-02-04 21:05:43 +0200

    login fontSize

--- src/components/auth/loginPage/login.jsx
+++ src/components/auth/loginPage/login.jsx
@@ -72,7 +72,7 @@ export default function Login() {
   return (
 
     <div>
-      <h2 className='s20 '>Log In</h2>
+      <h2 className='s22'>Log In</h2>
       <h6 className='welcomeText'>Welcome back! Please enter your details.</h6>
 
       <form>
