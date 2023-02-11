81fc9fc1a6fbeec317e138d754dc460a9d134ad6
Chaya Segal <chayale90@gmail.com>
2023-02-12 01:33:47 +0200

    pagenation of favorite

--- src/components/client/myFavorites/myFavoriteList.jsx
+++ src/components/client/myFavorites/myFavoriteList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { changeFavorites } from "../../../features/homeSlice"
+import useScroll from '../../../hooks/useScroll';
 import { API_URL, doApiGet } from '../../../services/apiService';
 import MyInfo from '../../../services/myInfo';
 import CheckUserComp from '../../auth/checkComps/checkUserComp';
@@ -11,26 +12,46 @@ export default function MyFavoriteList() {
 
   const dispatch = useDispatch();
   const [ar, setAr] = useState([]);
+  const [endScreen, setEndScreenFalse] = useScroll(600);
+  const [firstLoad, setFirstLoad] = useState(true)
+  const [page,setPage] = useState(1);
+  const [show,setShow] = useState("block")
 
 
   useEffect(() => {
     dispatch(changeFavorites({ val: "block" }))
-
-    //get all foods that i liked them
-    doApiFavoriteFood()
     return () => {
       dispatch(changeFavorites({ val: "none" }))
     }
-
   }, [])
 
+  useEffect(()=>{
+    //get all foods that i liked them
+    doApiFavoriteFood()
+  },[page])
+
+  useEffect(() => {
+    console.log("end screen hook")
+    // בודק אם הדף רק נטען ולא יפעיל את הפקודה
+    if (!firstLoad && endScreen) {
+      setPage(page + 1)
+    }
+    setFirstLoad(false);
+    // לנסות לעשות שמגיעים לסוף הדף 
+    // שיציג את ה10 הסרטונים הביאם שיתווספו לרשימה
+  }, [endScreen])
 
   const doApiFavoriteFood = async () => {
-    let url = API_URL + "/foods/myLikeFoods";
+    let url = API_URL + "/foods/myLikeFoods?page="+page;
     try {
       let resp = await doApiGet(url);
       console.log(resp.data);
-      setAr(resp.data)
+      setAr([...ar, ...resp.data])
+      // מחזיר את הטוגל של בדיקה אם אנחנו בסוף העמוד הגלילה
+      // בחזרה לפולס
+      setEndScreenFalse()
+      setShow("none")  
+     
     }
     catch (err) {
       console.log(err);
@@ -54,6 +75,7 @@ export default function MyFavoriteList() {
             />
           );
         })}
+        {endScreen && <h1 style={{display:show}} className='diaplay-1'>Loading...</h1>}
         {ar.length < 1 && <div className='display-6 text-center my-3' style={{ color: "#A435F0" }}>Loading...</div>}
 
       </div>
