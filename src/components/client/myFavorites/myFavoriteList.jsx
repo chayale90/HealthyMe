d354e5cbb53a4ada1b7ea96ba8178df6b68e7a6a
Chaya Segal <chayale90@gmail.com>
2023-02-14 16:29:14 +0200

    pagenation in myFavorite list

--- src/components/client/myFavorites/myFavoriteList.jsx
+++ src/components/client/myFavorites/myFavoriteList.jsx
@@ -2,65 +2,61 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
 import { changeFavorites } from "../../../features/homeSlice"
-import useScroll from '../../../hooks/useScroll';
 import { API_URL, doApiGet } from '../../../services/apiService';
-import MyInfo from '../../../services/myInfo';
 import CheckUserComp from '../../auth/checkComps/checkUserComp';
 import FoodFavoriteItem from './foodFavoriteItem';
+import useInfiniteScroll from 'react-infinite-scroll-hook';
 
 export default function MyFavoriteList() {
 
   const dispatch = useDispatch();
-  const [ar, setAr] = useState([]);
-  const [endScreen, setEndScreenFalse] = useScroll(600);
-  const [firstLoad, setFirstLoad] = useState(true)
+  const [items, setItems] = useState([]);
   const [page, setPage] = useState(1);
-  const [show, setShow] = useState("block")
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
 
-
-  useEffect(() => {
-    doApiFavoriteFood()
-  }, [page])
 
   useEffect(() => {
     dispatch(changeFavorites({ val: "block" }))
+    loadMore()
     return () => {
       dispatch(changeFavorites({ val: "none" }))
-      setAr([])
     }
-  }, [])
+  }, [hasNextPage])
 
-  useEffect(() => {
-    console.log("end screen hook")
-    if (!firstLoad && endScreen) {
-      setPage(page + 1)
-    }
-    setFirstLoad(false);
-  }, [endScreen])
 
-  const doApiFavoriteFood = async () => {
-    let url = API_URL + "/foods/myLikeFoods?page=" + page;
+  const loadMore = async () => {
+    setLoading(true);
+    setPage(prevPage => prevPage + 1);
     try {
+      let url = API_URL + "/foods/myLikeFoods?page=" + page;
       let resp = await doApiGet(url);
       console.log(resp.data);
-      setAr([...ar, ...resp.data])
-      //return the toggle of end of page to false
-      setEndScreenFalse()
-      setShow("none")
-    }
-    catch (err) {
+      setItems([...items, ...resp.data]);
+      setHasNextPage(resp.data.length == 0);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
+      setLoading(false);
       console.log(err);
-      toast.error("There problem, try again later")
+      toast.error("there problem ,try again later");
     }
   };
 
+  const { sentryRef } = useInfiniteScroll({
+    loading,
+    hasNextPage,
+    onLoadMore: loadMore,
+    disabled: !!error,
+    rootMargin: '0px 600px 0px 0px',
+  });
 
   return (
     <div className="container mt-5">
-      {/* <MyInfo /> */}
       <CheckUserComp />
       <div className="row justify-content-center">
-        {ar.map((item, i) => {
+        {items.map((item, i) => {
           return (
             <FoodFavoriteItem
               key={item._id}
@@ -69,7 +65,11 @@ export default function MyFavoriteList() {
             />
           );
         })}
-        {endScreen && <h1 style={{ display: show }} className='diaplay-1 text-center'>Loading...</h1>}
+        {(loading) && (
+          <div ref={sentryRef}>
+            <div className='text-center'>Loading...</div>
+          </div>
+        )}
       </div>
     </div>
   )
