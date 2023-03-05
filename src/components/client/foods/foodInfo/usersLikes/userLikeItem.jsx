f2c3123fa7cc083d7af8dcd552d2af5c2df0e469
Chaya Segal <chayale90@gmail.com>
2023-03-05 12:36:19 +0200

    following and myProfile link

--- src/components/client/foods/foodInfo/usersLikes/userLikeItem.jsx
+++ src/components/client/foods/foodInfo/usersLikes/userLikeItem.jsx
@@ -1,12 +1,14 @@
-import { Avatar } from '@mui/material'
+import { Avatar, Button } from '@mui/material'
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { setOpenUsersLikes } from "../../../../../features/dialogSlice"
+import { btnStyle, btnStyle2, btnStyle3 } from '../../../../../services/btnStyle';
 
 
 export default function UserLikeItem({ item }) {
     const dispatch = useDispatch();
+    const { user } = useSelector(myStore => myStore.userSlice);
 
     //if for the avatar image
     const srcImg = React.useMemo(() => {
@@ -18,7 +20,7 @@ export default function UserLikeItem({ item }) {
             return item.img_url;
         }
     }, [item]);
-    
+
     return (
         <div>
             <div className='d-flex w-100 justify-content-between'>
@@ -30,12 +32,18 @@ export default function UserLikeItem({ item }) {
                     />
                     <Link
                         onClick={() => { dispatch(setOpenUsersLikes({ val: false })) }}
-                        to={"/userProfile/" + item._id} className='ms-2 dark underLine mt-1'>
+                        to={(user._id == item._id) ? "/myProfile" : "/userProfile/" + item._id} className='ms-2 dark underLine mt-1'>
                         {item.name}
                     </Link>
                 </div>
 
-                <div className='ml-auto float-end justify-content-end mt-1'>Follower</div>
+                <div className='ml-auto float-end justify-content-end mt-1'>
+                    <div className='px-2' style={btnStyle2}>
+                        {
+                            user.followings.includes(item._id) ? "following" : "follow"
+                        }
+                    </div>
+                </div>
             </div>
             <hr className='col' />
         </div>
